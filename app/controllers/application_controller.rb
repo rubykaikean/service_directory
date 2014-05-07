c38a5155c5cae73201282670630d1604dc705d37
rubykaikean <khongkk89@gmail.com>
2014-05-07 16:17:17 +0800

    done registration type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,19 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :authenticate_admin!
   helper_method :user_is_admin
   helper_method :user_is_business!
 
   def user_is_business!
     current_user.user_type == "1"
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:password_confirmation, :password, :email, :user_type) }
+    #devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:current_password, :password_confirmation, :password, :display_name, :about, :job_category_id, :document) }
   end
 
   def user_is_admin
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,18 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
 
-	# def create
-	# 	# render :text => params.to_json
-	# 	user = User.new
-	# 	if params[:commit] == "Sign up as poster"
-	# 		user.user_type = "1"
-	# 		user.save
-	# 		redirect_to root_url , notice: "Sign in "
-	# 	elsif params[:commit] == "Sign up as seeker"
-	# 		user.user_type = "2"
-	# 		user.save
-	# 		redirect_to root_url
-	# 	end
-	# end
 
+	protected
+
+	def after_sign_up_path_for(resource)
+    root_url
+  end
 
 end
